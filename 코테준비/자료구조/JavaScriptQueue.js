eacb1b172bd341a9b34bbe142e2e265aa9b3137b
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-08-30 18:53:42 +0900

    큐 코드로 직접구현

---
+++ 코테준비/자료구조/JavaScriptQueue.js
@@ -0,0 +1,36 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+
+  dequeue() {
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+
+  peek() {
+    return this.items[this.headIndex];
+  }
+
+  getLength() {
+    return this.tailIndex - this.headIndex;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(5);
+queue.dequeue();
+queue.enqueue(4);
+queue.enqueue(3);
+queue.enqueue(2);
+
+console.log(queue);
