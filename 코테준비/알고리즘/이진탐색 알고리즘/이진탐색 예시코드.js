df40a78b0fdaaa6ffbc50e6954a3a395a63def7f
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-12 21:01:04 +0900

    이진탐색 예시코드

--- 코테준비/알고리즘/이진탐색 알고리즘/이진탐색 예시코드.js
+++ 코테준비/알고리즘/이진탐색 알고리즘/이진탐색 예시코드.js
@@ -1,7 +1,46 @@
+//이진 탐색 소스코드 구현
 function binarySearch(arr, target, start, end) {
   if (start > end) return -1;
   let mid = parseInt((start + end) / 2);
   if (arr[mid] === target) return mid;
   else if (arr[mid] > target) return binarySearch(arr, target, start, mid - 1);
   else return binarySearch(arr, target, mid + 1, end);
+}
+
+//반복문으로 구현
+function binarySearch2(arr, target, start, end) {
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] > target) end = mid - 1;
+    else start = mid + 1;
+  }
+  return -1;
+}
+
+//정렬된 순서 유지하면서 배열에 삽입할 가장 왼쪽 인덱스
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] >= target) end = mid;
+    else start = mid + 1;
+  }
+  return end;
+}
+
+//정렬된 순서 유지하면서 배열에 삽입할 가장 오른쪽 인덱스
+function upperBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    else start = mid + 1;
+  }
+  return end;
+}
+
+// 값이 [leftValue, rightValue] 인 데이터 갯수 반환하는 함수
+function countByRange(arr, leftValue, rightValue) {
+  let rightIndex = uppderBound(arr, rightValue, 0, arr.length);
+  let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
+  return rightIndex - leftIndex;
 }
