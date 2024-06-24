0e4c15f26ff876ac74d9c6dc50c08cec18caf098
김진현 <jinhyeon0810@gmail.com>
2024-06-25 08:59:23 +0900

    study 백준 백트래킹 & DFS

--- 코테준비/알고리즘/DFS/DFS 예시코드.js
+++ 코테준비/알고리즘/DFS/DFS 예시코드.js
@@ -5,10 +5,163 @@ function example_code(v) {
   function dfs(v) {
     visited[v] = true;
     for (x of graph[v]) {
-      console.log("graph[v]", graph[v]);
       if (!visited[x]) dfs(x);
     }
   }
   dfs(v);
 }
-console.log(example_code(1));
+//1. 2606 웜바이러스 문제
+function solution1() {
+  let fs = require("fs");
+  let input = fs.readFileSync("index.txt").toString().split("\n");
+  let num = Number(input[0]);
+  let connectedNum = Number(input[1]);
+
+  let graph = [];
+  for (let i = 0; i <= num; i++) graph.push([]);
+  for (let i = 2; i <= connectedNum + 1; i++) {
+    let [x, y] = input[i].split(" ").map(Number);
+    graph[x].push(y);
+    graph[y].push(x);
+  }
+
+  let visited = new Array(num).fill(false);
+  let cnt = 0;
+  function dfs(v) {
+    visited[v] = true;
+    cnt += 1;
+    for (let i of graph[v]) {
+      if (!visited[i]) dfs(i);
+    }
+  }
+  dfs(1);
+  let result = cnt - 1;
+}
+
+//2.백준 1012 유기농배추 문제
+// function solution2() {
+//   let fs = require("fs");
+//   let input = fs.readFileSync("index.txt").toString().split("\n");
+//   let testCases = Number(input[0]);
+//   let line = 1;
+
+//   function dfs(graph, n, m, x, y) {
+//     if (x <= -1 || x >= n || y <= -1 || y >= m) {
+//       return false;
+//     }
+
+//     if (graph[x][y] == 1) {
+//       graph[x][y] = -1;
+//       dfs(graph, n, m, x - 1, y);
+//       dfs(graph, n, m, x, y - 1);
+//       dfs(graph, n, m, x + 1, y);
+//       dfs(graph, n, m, x, y + 1);
+//       return true;
+//     }
+//     return false;
+//   }
+
+//   while (testCases--) {
+//     let [m, n, k] = input[line].split(" ").map(Number);
+//     let graph = [];
+//     for (let i = 0; i < n; i++) {
+//       graph[i] = new Array(m);
+//     }
+
+//     for (let i = 1; i <= k; i++) {
+//       let [y, x] = input[line + i].split(" ").map(Number);
+//       graph[x][y] = 1;
+//     }
+//     let answer = 0;
+
+//     for (let i = 0; i < n; i++) {
+//       for (let j = 0; j < m; j++) {
+//         if (dfs(graph, n, m, i, j)) {
+//           answer++;
+//           console.log("가로,세로", j, i);
+//           console.log("answering", answer);
+//         }
+//       }
+//     }
+//     line += k + 1;
+//     console.log("answer", answer);
+//   }
+// }
+
+//2. 백준 1012 유기농배추
+function solution2() {
+  let fs = require("fs");
+  let input = fs.readFileSync("index.txt").toString().split("\n");
+
+  let testCases = Number(input[0]);
+  let line = 1;
+
+  while (testCases--) {
+    let [m, n, k] = input[line].split(" ").map(Number);
+
+    let graph = [];
+    for (let i = 0; i < n; i++) {
+      graph[i] = new Array(m).fill(0);
+    }
+    for (let i = 1; i <= k; i++) {
+      let [x, y] = input[line + i].split(" ").map(Number);
+      graph[y][x] = 1;
+    }
+
+    let answer = 0;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (dfs(graph, m, n, i, j)) {
+          answer++;
+        }
+      }
+    }
+    line += k + 1;
+    console.log("answer", answer);
+  }
+
+  function dfs(graph, m, n, x, y) {
+    if (x < 0 || x >= m || y < 0 || y >= n) {
+      return false;
+    }
+    if (graph[y][x] === 1) {
+      graph[y][x] = -1;
+      dfs(graph, m, n, x - 1, y);
+      dfs(graph, m, n, x, y - 1);
+      dfs(graph, m, n, x + 1, y);
+      dfs(graph, m, n, x, y + 1);
+      return true;
+    }
+    return false;
+  }
+}
+
+//3. 백준 1240 노드사이의 거리 : 이해안됨
+function solution3() {
+  let fs = require("fs");
+  let input = fs.readFileSync("index.txt").toString().split("\n");
+  let [nodeNum, m] = input[0].split(" ").map(Number);
+  // 1: 2(2),4(3)
+  // 2: 1(2)
+  // 3: 4(2)
+  // 4: 3(2)
+  let graph = [];
+  for (let i = 0; i < nodeNum + 1; i++) {
+    graph[i] = [];
+  }
+
+  for (let i = 1; i < nodeNum; i++) {
+    let [x, y, cost] = input[i].split(" ").map(Number);
+    graph[x].push([y, cost]);
+    graph[y].push([x, cost]);
+  }
+
+  for (let i = 0; i < m; i++) {
+    let [x, y] = input[nodeNum + i].split(" ").map(Number);
+    // visited = new Array(n + 1).fill(false);
+  }
+
+  console.log("graph", graph);
+}
+
+console.log(solution3());
--- 코테준비/알고리즘/백트래킹/백트래킹 문제풀이.js
+++ 코테준비/알고리즘/백트래킹/백트래킹 문제풀이.js
@@ -29,6 +29,41 @@ function solution1() {
   dfs(0);
 }
 
+//1번 수열 복습
+function solution1_1() {
+  let fs = require("fs");
+  let input = fs.readFileSync("index.txt").toString().split(" ");
+  console.log("input", input);
+  let [n, m] = input.map(Number);
+  let arr = [];
+  for (let i = 1; i <= n; i++) arr.push(i);
+
+  let visited = new Array(n).fill(false);
+  let selected = [];
+  let answer = "";
+
+  function dfs(depth) {
+    if (depth === m) {
+      let result = [];
+      for (let x of selected) result.push(arr[x]);
+      for (let x of result) answer += x + " ";
+      answer += "\n";
+      console.log("answer", answer);
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if (visited[i]) continue;
+      selected.push(i);
+      visited[i] = true;
+      dfs(depth + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  }
+  dfs(0);
+}
+
 //2.10974 모든순열
 function solution2() {
   let fs = require("fs");
@@ -190,36 +225,108 @@ function solution6_1() {
 
   function dfs(depth) {
     if (depth === numOfCity - 1) {
+      console.log("이때depth", depth);
+      console.log("graph", graph);
       let totalCost = 0;
       let cur = 1;
       for (let i = 0; i < numOfCity - 1; i++) {
         let nextNode = selected[i];
         let cost = graph[cur][nextNode];
+        console.log("cur", cur);
+        console.log("next", nextNode);
         if (cost === 0) return;
+        console.log("cost", cost);
+
         totalCost += cost;
         cur = nextNode;
       }
       let cost = graph[cur][1];
+      console.log("cost2", cost);
+      console.log("after", graph);
       if (cost === 0) return;
       totalCost += cost;
+      console.log("cur", cur, "totalCost", totalCost);
+
+      console.log("seleceted", selected);
       minValue = Math.min(totalCost, minValue);
+      console.log("min", minValue);
       return minValue;
     }
 
     for (let i = 2; i <= numOfCity; i++) {
+      console.log("start", i, "depth", depth);
       if (visited[i]) continue;
       visited[i] = true;
       selected.push(i);
+      console.log("seling", selected);
+      console.log("visiting", visited);
       dfs(depth + 1);
+      console.log("here?", i);
+      console.log("heredepth>?", depth);
       visited[i] = false;
       selected.pop();
+
+      console.log("afterselected", selected);
+      console.log("aftervisited", visited);
     }
   }
   dfs(0);
   console.log(minValue);
 }
+// console.log(solution6_1());
 
-console.log(solution6_1());
+//외판원순회2
+function solution6_2() {
+  let fs = require("fs");
+  let input = fs.readFileSync("index.txt").toString().split("\n");
+  let cityNum = Number(input[0]);
+
+  let graph = [];
+  for (let i = 0; i <= cityNum; i++) {
+    graph.push([0]);
+  }
+
+  for (let i = 1; i <= cityNum; i++) {
+    let line = input[i].split(" ").map(Number);
+    graph[i].push(...line);
+  }
+
+  let visited = new Array(cityNum + 1).fill(false);
+  let selected = [];
+  let minValue = 1e9;
+  function dfs(m) {
+    if (m === cityNum) {
+      let totalCost = 0;
+      let startNode = 0;
+      let nextNode = 0;
+      for (let i = 0; i < cityNum - 1; i++) {
+        startNode = selected[i];
+        nextNode = selected[i + 1];
+        let cost = graph[startNode][nextNode];
+        if (cost === 0) return;
+        totalCost += cost;
+      }
+      let cost = graph[nextNode][selected[0]];
+      if (cost === 0) return;
+      totalCost += cost;
+      minValue = Math.min(minValue, totalCost);
+      console.log("min", minValue);
+      return;
+    }
+
+    for (let i = 1; i <= cityNum; i++) {
+      if (visited[i]) continue;
+      visited[i] = true;
+      selected.push(i);
+      dfs(m + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  }
+  dfs(0);
+}
+
+// console.log(solution6_1());
 
 // function solution6() {
 //   let fs = require("fs");
@@ -272,3 +379,47 @@ console.log(solution6_1());
 //   console.log("min", minValue);
 // }
 // console.log(solution6());
+
+//7. 도영이음식 백준 2961
+function solution7() {
+  //신것들의 곱 - 쓴것들의 합 의 최소를 구한다.
+  //1e9 설정하고 비교해줘서 min찾기
+  let fs = require("fs");
+  let input = fs.readFileSync("index.txt").toString().split("\n");
+
+  let number = Number(input[0]);
+
+  let graph = [];
+  for (let i = 1; i <= number; i++) {
+    let line = input[i].split(" ").map(Number);
+    graph.push(line);
+  }
+  let selected = [];
+  let visited = new Array(number).fill(false);
+  let minValue = 1e9;
+  function dfs(depth) {
+    if (depth > 0) {
+      let totalX = 1;
+      let totalY = 0;
+
+      for (let i of selected) {
+        let [x, y] = graph[i];
+        totalX *= x;
+        totalY += y;
+      }
+      minValue = Math.min(minValue, Math.abs(totalX - totalY));
+    }
+
+    for (let i = 0; i < number; i++) {
+      if (visited[i]) continue;
+      visited[i] = true;
+      selected.push(i);
+      dfs(depth + 1);
+      visited[i] = false;
+      selected.pop();
+    }
+  }
+  dfs(0);
+  console.log(minValue);
+}
+// console.log(solution7());
